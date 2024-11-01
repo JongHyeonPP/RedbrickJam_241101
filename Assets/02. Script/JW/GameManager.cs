0f78e0e231f6a04eab8a7a4135774a80b6d6f0e1
jiwon <heich4088@naver.com>
2024-11-02 02:21:44 +0900

    저장기능 구현 완료

--- Assets/02. Script/JW/GameManager.cs
+++ Assets/02. Script/JW/GameManager.cs
@@ -1,12 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public int stage;
+    float mainVolume;
+    float bgm;
+    float sfx;
 
-    private void Awake()
+    private void Start()
     {
         SoundManager.instance.FindSlider();
     }
+
+    public void NextStage()
+    {
+        stage++;
+        SaveStage();
+    }
+
+    public void SaveStage()
+    {
+        PlayerPrefs.SetInt("Stage", stage);
+    }
+
+    public void LoadStage()
+    {
+        if (PlayerPrefs.HasKey("Stage"))
+        {
+            stage = PlayerPrefs.GetInt("Stage");
+            Debug.Log(stage);
+        }
+    }
+
 }

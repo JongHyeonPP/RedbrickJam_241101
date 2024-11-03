56f8142b3e0a14668df206ff36418203d9ccc2f1
jiwon <heich4088@naver.com>
2024-11-03 16:48:44 +0900

    ..

--- Assets/02. Script/JW/ClearZone.cs
+++ Assets/02. Script/JW/ClearZone.cs
@@ -14,7 +14,7 @@ public class ClearZone : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && GameManager.instance.gem1 == 1 && GameManager.instance.gem2 == 1 && GameManager.instance.gem3 == 1)
         {
             Time.timeScale = 0;
             clearScreen.SetActive(true);
@@ -27,6 +27,7 @@ public class ClearZone : MonoBehaviour
         if (isClear && Input.GetMouseButtonDown(0))
         {
             Time.timeScale = 1;
+            isClear = false;
             ButtonManager.instance.MoveScene("Lobby");
         }
     }
